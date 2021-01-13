82a92c32744fb3db3ba242491a05857c5abccfdd
Ifan Sulistiyo <111201911988@mhs.dinus.ac.id>
2021-01-13 10:27:44 +0700

    latiansendirirepo12

---
+++ for.js
@@ -0,0 +1,16 @@
+//console.log('hallo');
+//for( nilaiawal = 1 ; nilaiawal <= 10; nilaiawal++){
+	//console.log('Angkot No. ' +  nilaiawal + ' beroprasi dengan baik ' );
+//}
+
+jumlahangkot = 10;
+noangkot = 1;
+angkotberoprasi = 6;
+while(noangkot <= angkotberoprasi){
+	console.log('Angkot No ' + noangkot + ' beroprasi dengan baik ');
+	noangkot++
+
+}
+for(noangkot = angkotberoprasi +1; noangkot<=  jumlahangkot; noangkot++){
+	console.log('Angkot No ' + noangkot + 'sedang dalam trouble ')
+}
