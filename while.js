57253aa31b2084152698803e709d57a287c3b44d
Ifan Sulsitiyo <111201911988@mhs.dinus.ac.id>
2021-01-13 10:24:28 +0700

    latiansendirirepo11

---
+++ while.js
@@ -0,0 +1,19 @@
+console.log('hello world');
+//lopping forever akan menalankan terus menerus
+//while( true ){
+//console.log("hello");
+//}
+
+//tidak akan terjadi apa apa karena false
+while( false ){
+console.log("hello");
+}
+
+//cara menghentikan looping forever dengan confirm
+ulang = true;
+while(ulang){
+	console.log('hellow world');
+	ulang = confirm('lagi');
+}
+
+
